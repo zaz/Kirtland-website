3a5b65b0e9660466656f9528d77a2e85c4676932
Zaz Brown <zazbrown@zazbrown.com>
2016-12-16 16:27:14 -0500

    main.js: formatting

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 function adjust_header(header) {
-			let scroll = $(window).scrollTop()
-			if (100 > scroll) {
-				header.height(128 - scroll)
-				header.css("padding", "16px")
-			} else if (124 > scroll) {
-				header.css("padding", 16 + (100 - scroll)/2 + "px")
-			} else {
-				header.height(28)
-				header.css("padding", "4px")
-			}
-			if (scroll > 80) { header.addClass("hide-detail") }
-			else { header.removeClass("hide-detail") }
+	let scroll = $(window).scrollTop()
+	if (100 > scroll) {
+		header.height(128 - scroll)
+		header.css("padding", "16px")
+	} else if (124 > scroll) {
+		header.css("padding", 16 + (100 - scroll)/2 + "px")
+	} else {
+		header.height(28)
+		header.css("padding", "4px")
+	}
+	if (scroll > 80) { header.addClass("hide-detail") }
+	else { header.removeClass("hide-detail") }
 }
 
 $( () => {
-		let header = $("header")
+	let header = $("header")
 
-		$(window).scroll( e => adjust_header(header) )
-		adjust_header(header)
+	$(window).scroll( e => adjust_header(header) )
+	adjust_header(header)
 })
