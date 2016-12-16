65d8e2b8cc3f0ff05255bca3d55e800ea978b421
Zaz Brown <zazbrown@zazbrown.com>
2016-12-16 16:29:10 -0500

    in-page links: scroll smoothly

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const animation_period = 300
+
 function adjust_header(header) {
 	let scroll = $(window).scrollTop()
 	if (100 > scroll) {
@@ -13,9 +15,24 @@ function adjust_header(header) {
 	else { header.removeClass("hide-detail") }
 }
 
+function in_page_links_smooth_scroll() {
+	$('a[href^="#"]').click(function (e) {
+		e.preventDefault()
+
+		let target = this.hash
+		let offset = $(target).offset().top
+
+		$('html, body').stop().animate({ scrollTop: offset },
+			animation_period, 'swing', () =>
+				window.location.hash = target
+		)
+	})
+}
+
 $( () => {
 	let header = $("header")
 
 	$(window).scroll( e => adjust_header(header) )
 	adjust_header(header)
+	in_page_links_smooth_scroll()
 })
