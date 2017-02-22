b268149d43c8c96b185e2048cb8a7dd2065f8822
Zaz Brown <zazbrown@zazbrown.com>
2017-02-22 15:52:33 -0500

    color to orange; header shrink on scroll; merge Caroline
    introduces JavaScript

---
+++ main.js
@@ -0,0 +1,22 @@
+function adjust_header(header) {
+			scroll = document.body.scrollTop
+			if (100 > scroll) {
+				console.log(scroll)
+				header.height(128 - scroll)
+				header.css("padding", "16px")
+			} else if (124 > scroll) {
+				header.css("padding", 16 + (100 - scroll)/2 + "px")
+			} else {
+				header.height(28)
+				header.css("padding", "4px")
+			}
+			if (scroll > 80) { header.addClass("hide-detail") }
+			else { header.removeClass("hide-detail") }
+}
+
+$( () => {
+		let header = $("header")
+
+		$(window).scroll( e => adjust_header(header) )
+		adjust_header(header)
+})
