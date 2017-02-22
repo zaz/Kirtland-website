e911d8bb0b08332a87a8f9d59ca606fa3292ea89
Zaz Brown <zazbrown@zazbrown.com>
2017-02-22 15:56:12 -0500

    -flip effect; * form; orange -> blue; * header

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 const animation_period = 300
 
-function adjust_header(header) {
-	let scroll = $(window).scrollTop()
-	if (100 > scroll) {
-		header.height(128 - scroll)
-		header.css("padding", "16px")
-	} else if (124 > scroll) {
-		header.css("padding", 16 + (100 - scroll)/2 + "px")
-	} else {
-		header.height(28)
-		header.css("padding", "4px")
-	}
-	if (scroll > 80) { header.addClass("hide-detail") }
-	else { header.removeClass("hide-detail") }
-}
-
 function in_page_links_smooth_scroll() {
 	$('a[href^="#"]').click(function (e) {
 		e.preventDefault()
@@ -30,9 +15,5 @@ function in_page_links_smooth_scroll() {
 }
 
 $( () => {
-	let header = $("header")
-
-	$(window).scroll( e => adjust_header(header) )
-	adjust_header(header)
 	in_page_links_smooth_scroll()
 })
